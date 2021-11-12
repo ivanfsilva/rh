0e54b9063ae5804f6343a5737b4ca4a54d9cff87
ivanfsilva <ivanfs27@gmail.com>
2021-11-12 00:01:36 -0300

    extracting validations

---
+++ backend/src/main/java/br/com/ivanfsilva/rh/service/ValidacaoReajuste.java
@@ -0,0 +1,10 @@
+package br.com.ivanfsilva.rh.service;
+
+import br.com.ivanfsilva.rh.model.Funcionario;
+
+import java.math.BigDecimal;
+
+public interface ValidacaoReajuste {
+
+    void validar(Funcionario funcionario, BigDecimal aumento );
+}
